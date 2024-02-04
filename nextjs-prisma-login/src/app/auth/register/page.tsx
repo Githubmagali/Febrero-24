c974285c35d61b6f580e40a127a92edbf194c300
Githubmagali <magalivictoria85068@gmail.com>
2024-02-04 17:23:03 -0300

    cambios en el registro autorizaciones

--- nextjs-prisma-login/src/app/auth/register/page.tsx
+++ nextjs-prisma-login/src/app/auth/register/page.tsx
@@ -1,82 +1,105 @@
 "use client" //todo va a ser procesado por el navegador
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 
-function RegisterPage(){
-const {register, handleSubmit, formState: {errors}} = useForm();
 
-const onSubmit = handleSubmit(data =>{
-    console.log(data)
-})
+function RegisterPage() {
+    const { register, handleSubmit, formState: { errors }, } = useForm();
 
-    return(
+    const onSubmit = handleSubmit(async (data) => {
+
+        if (data.password !== data.confirmPassword) {
+            return alert("Password do not match");
+        }
+
+
+        const res = await fetch('/api/auth/register', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: data.username,
+                email: data.email,
+                password: data.password,
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        //const dataJSON = await res.json()
+        //console.log(dataJSON)
+
+        if (res.ok) {
+            window.location.href = '/'; 
+        }
+    });
+
+    return (
         <div className="flex justify-center items-center h-screen">
             <form className="bg-white p-6 rounded shadow-md flex flex-col w-96"
-             action=""
-             onSubmit={onSubmit}>
+                action=""
+                onSubmit={onSubmit}>
                 <h1 className='text-center font-bold hover:text-neutral-500'>Register</h1>
-                    <input type="text" 
+                <input type="text"
                     placeholder='Username'
-                    className="mb-4 px-3 py-2 border rounded"
-                    {...(register("username",{
-                    required:{
-                        value: true,
-                        message: 'Username is required'
-                    }
+                    className="mt-3 px-3 py-2 border rounded"
+                    {...(register("username", {
+                        required: {
+                            value: true,
+                            message: 'Username is required'
+                        }
                     }))} />
-                    {
-                        errors.username && (
-                            <div className='text-red-500 '
-                            >{errors.username.message}</div>
-                        )
-                    }
-                    <input type="email" 
+                {
+                    errors.username && (
+                        <div className='text-red-500 '
+                        >{errors.username.message}</div>
+                    )
+                }
+                <input type="email"
                     placeholder='Email'
-                    className="mb-4 px-3 py-2 border rounded"
-                    {...(register("email",{
-                    required:{
-                        value: true,
-                        message: 'Email is required'
-                    }
+                    className="mt-3 px-3 py-2 border rounded"
+                    {...(register("email", {
+                        required: {
+                            value: true,
+                            message: 'Email is required'
+                        }
                     }))} />
-                      {
-                        errors.email && (
-                            <div className='text-red-500 '
-                            >{errors.email.message}</div>
-                        )
-                    }
-                    <input type="password"
+                {
+                    errors.email && (
+                        <div className='text-red-500 '
+                        >{errors.email.message}</div>
+                    )
+                }
+                <input type="password"
                     placeholder='Password'
-                    className="mb-4 px-3 py-2 border rounded"
-                    {...(register("password",{
-                        required:{
+                    className="mt-3 px-3 py-2 border rounded"
+                    {...(register("password", {
+                        required: {
                             value: true,
                             message: 'Password is required'
                         }
                     }))} />
-                    {
-                        errors.password && (
-                            <div className='text-red-500 '
-                            >{errors.password.message}</div>
-                        )
-                    }
-                    <input type="confirmPassword"
+                {
+                    errors.password && (
+                        <div className='text-red-500'
+                        >{errors.password.message}</div>
+                    )
+                }
+                <input type="confirmPassword"
                     placeholder='Repeat password'
-                    className="mb-4 px-3 py-2 border rounded"
-                    {...register("confirmPassword",{
-                        required:{
+                    className="mt-3 px-3 py-2 border rounded"
+                    {...register("confirmPassword", {
+                        required: {
                             value: true,
                             message: 'Confirm password is required'
                         }
                     })} />
-                    {
-                        errors.confirmPassword && (
-                            <div className='text-red-500 '
-                            >{errors.confirmPassword.message}</div>
-                        )
-                    }
-                    <button className='bg-neutral-400 px-4 py-2 rounded hover:bg-neutral-500'>
-                        Register</button>
+                {
+                    errors.confirmPassword && (
+                        <div className='text-red-500 '
+                        >{errors.confirmPassword.message}</div>
+                    )
+                }
+                <button className='mt-3 bg-neutral-400 px-4 py-2 rounded hover:bg-neutral-500'>
+                    Register</button>
             </form>
         </div>
     )
--- nextjs-prisma-login/src/libs/db.js
+++ nextjs-prisma-login/src/libs/db.js
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => {
   return new PrismaClient()
 }
+const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
